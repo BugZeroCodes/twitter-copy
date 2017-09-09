8ecb2ea1b15370574fdd1a78591009c497f7f527
Timothy Amadi <timothy@Timothys-MacBook-Air.local>
2017-09-09 10:21:29 -0400

    Psycho!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728173923) do
+ActiveRecord::Schema.define(version: 20170908213230) do
 
   create_table "tags", force: :cascade do |t|
     t.string "phrase"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170728173923) do
     t.text "following"
     t.string "tag"
     t.string "color"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
